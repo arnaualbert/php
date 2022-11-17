cf8f17ffdd74975f4eaa290f579c96f7e0ea92ca
dax <arnaualbert2003@gmail.com>
2022-11-17 17:46:26 +0100

    dog and cat

--- heredar/animal.php
+++ heredar/animal.php
@@ -1,9 +1,22 @@
 <?php
 require_once "./interface.php";
-class Animal implements Speaker{
+abstract class Animal implements Speaker{
+    
+    public function __construct(public string $name){
+        $this->name=$name;
+    }
 
+    public function getname(){
+        return $this->name;
+    }
 
-    public function talk(){
-        echo "tictac";
+    public function setname(){
+        $this->name=$name;
+    }
+
+    public abstract function talk();
+
+    public function __tostring(){
+        return sprintf("%s(name:%s)",get_class($this),$this->name);
     }
 }
