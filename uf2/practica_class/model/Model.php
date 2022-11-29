96bd3de72e133a7a9bb5bcb9751ddc798ab720bf
dax <arnaualbert2003@gmail.com>
2022-11-29 20:47:51 +0100

    practica_calss v1.0

--- uf2/practica_class/model/Model.php
+++ uf2/practica_class/model/Model.php
@@ -4,8 +4,15 @@ class Model{
     /**
      * search all the products in the porducts list or an empty array if not found or an error
      */
-    public function searchAllProducts(){
+    public function searchAllProducts(): ?array{
         //return array();
         return Null;
+    }
+
+    /**
+     * search all users from data source  or an empty array if not found or an error
+     */
+    public function searchAllUsers(): ?array{
+
     }
 }
