66815ee4930e5735e06ecd2432d0e26830917b87
dax <arnaualbert2003@gmail.com>
2022-12-20 20:37:08 +0100

    practiva v5

--- uf2/practica_class/views/logout.php
+++ uf2/practica_class/views/logout.php
@@ -1,13 +1,3 @@
-<script type="text/javascript">
-function submitForm(event) {
-    var target = event.target;
-    var buttonId = target.id;
-    var myForm = document.getElementById('item-form');
-    myForm.action.value = buttonId;
-    myForm.submit();
-    return false;
-}
-</script> 
 <?php
 
 $session = session_status() === PHP_SESSION_ACTIVE ? TRUE : FALSE;
@@ -28,9 +18,7 @@ echo <<<EOT
     <form id="item-form" method="post" action="index.php">
 
      <label for="logout">Logout?</label>
-    <button type="button" id="logoutyes" name="logoutyes"  onclick="submitForm(event);return false;">Yes</button>
-    <input name="action" id="action" hidden="hidden" value="add"/>
-
+    <button type="submit" id="logoutyes" name="action" value="logoutyes">Yes</button>
  </form>
 EOT;}
 ?>
